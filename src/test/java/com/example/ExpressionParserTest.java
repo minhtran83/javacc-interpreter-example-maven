12a25cdaff3bb19cf1f9f80d0525290a2a8f5d27
Minh Tran <mtran@atlassian.com>
2024-05-09 16:33:14 +1000

    Refactor some code

--- src/test/java/com/example/ExpressionParserTest.java
+++ src/test/java/com/example/ExpressionParserTest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.Test;
 
 import java.io.StringReader;
-import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.entry;
 import static org.assertj.core.api.MapAssert.assertThatMap;
@@ -23,7 +23,8 @@ public class ExpressionParserTest {
         );
         ASTstart expr = ExpressionParser.start();
         ExpressionVisitor v = new ExpressionVisitor();
-        assertThatMap((HashMap) expr.jjtAccept(v, null)).containsAnyOf(
+        Map<String, Object> result = (Map<String, Object>) expr.jjtAccept(v, null);
+        assertThatMap(result).containsAnyOf(
                 entry("a", 6),
                 entry("b", 18),
                 entry("c", 24),
